10027c0382fdc71799c4b65d4677344703829e9c
Pedro Hernandez <phpleo@gmail.com>
2009-12-25 12:05:12 -0500

    - actualizacion a sf1.4

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,13 +1,12 @@
 <?php
 
-require_once '/opt/symfony/sf1.2/lib/autoload/sfCoreAutoload.class.php';
+require_once '/opt/symfony/sf1.4/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-    // for compatibility / remove and enable only the plugins you want
-    $this->enableAllPluginsExcept(array('sfDoctrinePlugin', 'sfCompat10Plugin'));
+    $this->enablePlugins('sfDoctrinePlugin');
   }
 }
---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    sympress
+ * @subpackage form
+ * @author     Pedro Hernandez <phpleo> 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
